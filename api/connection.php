040ae8f476a6356214b1af2e02a8dbde32abd9cf
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-09-28 13:38:06 -0300

    fix file of configuration

--- api/connection.php
+++ api/connection.php
@@ -5,8 +5,7 @@ $DB_USER=$_ENV["DB_USER"];
 $DB_PASSWORD=$_ENV["DB_PASSWORD"];
 $DB_NAME=$_ENV["DB_NAME"];
 $DB_PORT=$_ENV["DB_PORT"];
-$DB_HOST=$_ENV["DB_HOST"];
 
-$db=mysqli_connect("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT", "$DB_HOST");
+$db=mysqli_connect("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
 
 ?>
