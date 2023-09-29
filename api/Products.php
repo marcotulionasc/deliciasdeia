9e917425041484e8953fb3407ccb6dcf41b64059
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-09-28 22:25:31 -0300

    img tag inside php

--- api/Products.php
+++ api/Products.php
@@ -12,6 +12,7 @@ if ($result) {
         echo '<div class="col-lg-3 col-md-6 col-sm-6">';
         echo '    <div class="product__item">';
         echo '        <div class="product__item__pic set-bg" data-setbg="' . $row['image'] . '">';
+        echo '            <img src="' . $row['image'] . '" alt="teste">';
         echo '            <div class="product__label">';
         echo '                <span>' . $row['categoryName'] . '</span>';
         echo '            </div>';
