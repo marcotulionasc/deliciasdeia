e5692bbefe9babd820b9e076e41ae760eb3bde3c
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-10-04 13:10:47 -0300

    fix shop cart 7

--- api/processShopCart.php
+++ api/processShopCart.php
@@ -2,10 +2,10 @@
 require_once 'connection.php'; // Certifique-se de que a conexão com o banco de dados esteja estabelecida (variável $db)
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $idProduto = $_POST["idProduto"];
+    $idProduct = $_POST["idProduct"];
 
     // Suponha que a tabela 'Order' tenha colunas 'order_id' (auto-incremento) e 'product_id'
-    $sql = "INSERT INTO OrderProducts (idProduct) VALUES ('$idProduto')";
+    $sql = "INSERT INTO OrderProducts (idProduct) VALUES ('$idProduct')";
 
     if (mysqli_query($db, $sql)) {
         echo json_encode(["success" => true]);
@@ -16,4 +16,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo json_encode(["success" => false]);
 }
 
+
+$orderId = $_SESSION['order_id']; // Certifique-se de ter essa variável definida corretamente
+
+// Consulta para obter os produtos em um pedido específico
+$sql = "SELECT P.* FROM Products P
+        INNER JOIN OrderProducts O ON P.idProduct = O.idProduct
+        WHERE O.idOrder = $orderId";
+
+$result = mysqli_query($db, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td class='product__cart__item'>";
+        echo "<img src='api/displayImage.php?produto_id=" . $row['idProduct'] . "'>";
+        echo "<div class='product__cart__item__text'>";
+        echo "<h6>" . $row['nameProduct'] . "</h6>";
+        echo "<h5>" . $row['price'] . "</h5>";
+        echo "</div>";
+        echo "</td>";
+        echo "<td class='quantity__item'>";
+        echo "<div class='quantity'>";
+        echo "<div class='pro-qty'>";
+        echo "<input type='text' value='1'>";
+        echo "</div>";
+        echo "</div>";
+        echo "</td>";
+        echo "<td class='cart__price'>$ 30.00</td>";
+        echo "<td class='cart__close'><span class='icon_close'></span></td>";
+        echo "</tr>";
+    }
+} else {
+    echo "Nenhum produto encontrado no pedido.";
+}
 ?>
