0911658cb7d715121e78cf7c71c346466c6fc0e7
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-10-15 11:02:27 -0300

    Trying a pg_connect

--- api/connection.php
+++ api/connection.php
@@ -1,11 +1,14 @@
 <?php
 session_start();
-$DB_HOST=$_ENV["DB_HOST"];
-$DB_USER=$_ENV["DB_USER"];
-$DB_PASSWORD=$_ENV["DB_PASSWORD"];
-$DB_NAME=$_ENV["DB_NAME"];
-$DB_PORT=$_ENV["DB_PORT"];
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
 
-$db=mysqli_connect("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
+$db = pg_connect("host=$DB_HOST dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD port=$DB_PORT");
 
+if (!$db) {
+    die("Falha na conexão: " . pg_last_error());
+}
 ?>
