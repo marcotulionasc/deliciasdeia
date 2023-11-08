c3559a478b785fa56b93df1c02f1050c31f14b25
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-11-08 10:56:32 -0300

    cart

--- api/shoping-cart.php
+++ api/shoping-cart.php
@@ -6,6 +6,7 @@ if (isset($_COOKIE['carrinho'])) {
 } else {
     $carrinho = array();
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -211,7 +212,7 @@ if (isset($_COOKIE['carrinho'])) {
                                 <!-- Fim da lógica do carrinho -->
                             </tbody>
                         </table>
-                    </div>
+                    </div>  
                     <div class="row">
                         <div class="col-lg-6 col-md-6 col-sm-6">
                             <div class="continue__btn">
@@ -332,10 +333,14 @@ if (isset($_COOKIE['carrinho'])) {
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <script>
- 
+                             
     // Aqui eu fiz uma var que controla se o desconto foi aplicado
     let discountApplied = false;
 
+    // Função para atualizar o carrinho de compras no armazenamento local (localStorage)
+    function updateCartInLocalStorage() {
+        localStorage.setItem('cart', JSON.stringify(cartData));
+    }
     // Função para calcular o subtotal e o total do carrinho
     function calcularTotal() {
         let subtotal = 0;
@@ -357,6 +362,7 @@ if (isset($_COOKIE['carrinho'])) {
             const total = subtotal - discountAmount;
             $('.cart__total li:first span').text('R$ ' + discountAmount);
             totalElement.text('R$ ' + total.toFixed(2));
+            
         }
     }
 
@@ -367,97 +373,68 @@ if (isset($_COOKIE['carrinho'])) {
         const newPrice = productPrice * quantity;
         priceElement.text(`R$ ${newPrice.toFixed(2)}`);
 
-        // Função para calcular o subtotal e o total do carrinho
-        function calcularTotal() {
-            let subtotal = 0;
+        // Atualizar o preço no armazenamento local (localStorage)
+        const cartData = JSON.parse(localStorage.getItem('cart')) || {};
+        cartData[productId] = quantity;
+        localStorage.setItem('cart', JSON.stringify(cartData));
 
-            // Loop através de cada item no carrinho
-            $('.cart__price').each(function () {
-                const price = parseFloat($(this).data('product-price'));
-                const quantity = parseInt($(this).closest('tr').find('.quantity-input').val());
-                const itemTotal = price * quantity;
-                subtotal += itemTotal;
-            });
+        // Após atualizar o preço, recalcule o total
+        calcularTotal();
+    }
 
-            // Atualize o subtotal e o total na página
-            const totalElement = $('.cart__total li:last span');
-            totalElement.text('R$ ' + subtotal.toFixed(2));
+    function applyCoupon(couponCode) {
+    if (!discountApplied && couponCode === 'MARCO2023') { // Verifica se o desconto ainda não foi aplicado
+        // Defina aqui o valor do desconto
+        const discountAmount = 10; // Substitua pelo valor do desconto desejado
+        // Recalcule o total com o desconto
+        const subtotalElement = $('.cart__total li:last span');
+        const subtotal = parseFloat(subtotalElement.text().replace('R$ ', ''));
+        const total = subtotal - discountAmount;
+        // Atualize o total na página
+        $('.cart__total li:first span').text('R$ ' + discountAmount);
+        subtotalElement.text('R$ ' + total.toFixed(2));
+        // Marque o desconto como aplicado
+        discountApplied = true;
 
-            if (discountApplied) {
-                const discountAmount = 10; // Substitua pelo valor do desconto desejado
-                const total = subtotal - discountAmount;
-                $('.cart__total li:first span').text('R$ ' + discountAmount);
-                totalElement.text('R$ ' + total.toFixed(2));
-            }
-        }
-
-        // Função para atualizar o preço ao alterar a quantidade
-        function updatePrice(productId, quantity) {
-            const priceElement = $(`.cart__price[data-product-id="${productId}"]`);
-            const productPrice = parseFloat(priceElement.data('product-price'));
-            const newPrice = productPrice * quantity;
-            priceElement.text(`R$ ${newPrice.toFixed(2)}`);
-
-            // Atualizar o preço no armazenamento local (localStorage)
-            const cartData = JSON.parse(localStorage.getItem('cart')) || {};
-            cartData[productId] = quantity;
-            localStorage.setItem('cart', JSON.stringify(cartData));
-
-            // Após atualizar o preço, recalcule o total
-            calcularTotal();
-        }
-
-        function applyCoupon(couponCode) {
-            if (!discountApplied && couponCode === 'MARCO2023') { // Verifica se o desconto ainda não foi aplicado
-                // Defina aqui o valor do desconto
-                const discountAmount = 10; // Substitua pelo valor do desconto desejado
-                // Recalcule o total com o desconto
-                const subtotalElement = $('.cart__total li:last span');
-                const subtotal = parseFloat(subtotalElement.text().replace('R$ ', ''));
-                const total = subtotal - discountAmount;
-                // Atualize o total na página
-                $('.cart__total li:first span').text('R$ ' + discountAmount);
-                subtotalElement.text('R$ ' + total.toFixed(2));
-                // Marque o desconto como aplicado
-                discountApplied = true;
-            } else if (discountApplied) {
-                alert('O desconto já foi aplicado.');
-            } else {
-                alert('Cupom de desconto inválido');
-            }
-        }
+    } else if (discountApplied) {
+        alert('O desconto já foi aplicado.');
+    } else {
+        alert('Cupom de desconto inválido');
+    }
+}
         // Adicione um manipulador de eventos para o botão "Remover"
-        $('.remove-button').on('click', function () {
-            const productId = $(this).data('product-id');
+         $('.remove-button').on('click', function () {
+        const productId = $(this).data('product-id');
+        // Remova o produto do carrinho no armazenamento local (localStorage)
+        const cartData = JSON.parse(localStorage.getItem('cart')) || {};
+        delete cartData[productId];
+        localStorage.setItem('cart', JSON.stringify(cartData));
+        
+        // Remova a linha da tabela do carrinho
+        $(this).closest('tr').remove();
+        delete cartData[productId];
+        
+        // Recalcule o total após remover o produto
+        calcularTotal();
+        
+});
 
-            // Remova o produto do carrinho no armazenamento local (localStorage)
-            const cartData = JSON.parse(localStorage.getItem('cart')) || {};
-            delete cartData[productId];
-            localStorage.setItem('cart', JSON.stringify(cartData));
+    // Lidar com o envio do formulário de cupom de desconto
+    $('form').submit(function (e) {
+        e.preventDefault();
+        const couponCode = $('input[type="text"]').val();
+        applyCoupon(couponCode);
+    });
 
-            // Remova a linha da tabela do carrinho
-            $(this).closest('tr').remove();
+    // Lidar com a mudança na quantidade
+    $('.quantity-input').on('change', function () {
+        const productId = $(this).data('product-id');
+        const newQuantity = parseInt($(this).val());
+        updatePrice(productId, newQuantity);
+    });
 
-            // Recalcule o total após remover o produto
-            calcularTotal();
-        });
-
-        // Lidar com o envio do formulário de cupom de desconto
-        $('form').submit(function (e) {
-            e.preventDefault();
-            const couponCode = $('input[type="text"]').val();
-            applyCoupon(couponCode);
-        });
-
-        // Lidar com a mudança na quantidade
-        $('.quantity-input').on('change', function () {
-            const productId = $(this).data('product-id');
-            const newQuantity = parseInt($(this).val());
-            updatePrice(productId, newQuantity);
-        });
-
-    // Recuperar o estado do carrinho do armazenamento local (localStorage)
-    const cartData = JSON.parse(localStorage.getItem('cart')) || {};
+     // Recuperar o estado do carrinho do armazenamento local (localStorage)
+     const cartData = JSON.parse(localStorage.getItem('cart')) || {};
     for (const productId in cartData) {
         const quantity = cartData[productId];
         updatePrice(productId, quantity);
@@ -465,8 +442,7 @@ if (isset($_COOKIE['carrinho'])) {
 
     // Chame a função de cálculo inicial para definir o valor correto na primeira carga da página
     calcularTotal();
-
-
+    
 </script>
 
 </body>
