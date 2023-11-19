da16a4d7277dd3e77e50fd090f40151eb6ba3e39
marcotulionasc <marconascpriv@gmail.com>
2023-11-19 17:35:49 -0300

    chatgpt ta de marola meno

--- api/Products.php
+++ api/Products.php
@@ -33,7 +33,23 @@ $result = $stmt->get_result();
 if ($result) {
     echo '<div class="row">';
     while ($row = $result->fetch_assoc()) {
-        // ... (código para exibir os produtos)
+        echo '<div class="col-lg-3 col-md-6 col-sm-6">';
+        echo '    <div class="product__item">';
+        echo '        <div class="product__item__pic set-bg">';
+        echo '            <img src="api/displayImage.php?produto_id=' . $row['idProduct'] . '" alt="' . $row['nameProduct'] . '">';
+        echo '            <div class="product__label">';
+        echo '                <span>' . $row['categoryName'] . '</span>';
+        echo '            </div>';
+        echo '        </div>';
+        echo '        <div class="product__item__text">';
+        echo '            <h6><a href="#">' . $row['nameProduct'] . '</a></h6>';
+        echo '            <div class="product__item__price">R$ ' . $row['price'] . '</div>';
+        echo '            <div class="cart_add">';
+        echo '<a href="api/addProduct.php?id=' . $row['idProduct'] . '">Adicionar ao carrinho</a>';
+        echo '            </div>';
+        echo '        </div>';
+        echo '    </div>';
+        echo '</div>';
     }
     echo '</div>';
 
