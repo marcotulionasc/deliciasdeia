6ac57b8ac249b9f9e7a13533fa36089938448209
marcotulionasc <marconascpriv@gmail.com>
2023-11-19 17:33:34 -0300

    Considerando que o active é booleano

--- api/Products.php
+++ api/Products.php
@@ -16,44 +16,30 @@ if ($paginaAtual < 1) {
     $paginaAtual = 1;
 }
 
+// Consulta SQL para obter o número total de produtos ativos
+$totalProdutos = $db->query("SELECT COUNT(*) as total FROM Products WHERE active = TRUE")->fetch_assoc()['total'];
+
 // Calcular o offset (deslocamento) com base na página atual
 $offset = ($paginaAtual - 1) * $itensPorPagina;
 
 // Consulta SQL para obter os produtos ativos paginados
-$query = "SELECT * FROM Products WHERE active=1 LIMIT $itensPorPagina OFFSET $offset";
-$result = $db->query($query);
+$query = "SELECT * FROM Products WHERE active = TRUE LIMIT ? OFFSET ?";
+$stmt = $db->prepare($query);
+$stmt->bind_param("ii", $itensPorPagina, $offset);
+$stmt->execute();
+$result = $stmt->get_result();
 
 // Verifica se a consulta foi bem-sucedida
 if ($result) {
     echo '<div class="row">';
     while ($row = $result->fetch_assoc()) {
-        echo '<div class="col-lg-3 col-md-6 col-sm-6">';
-        echo '    <div class="product__item">';
-        echo '        <div class="product__item__pic set-bg">';
-        echo '            <img src="api/displayImage.php?produto_id=' . $row['idProduct'] . '" alt="' . $row['nameProduct'] . '">';
-        echo '            <div class="product__label">';
-        echo '                <span>' . $row['categoryName'] . '</span>';
-        echo '            </div>';
-        echo '        </div>';
-        echo '        <div class="product__item__text">';
-        echo '            <h6><a href="#">' . $row['nameProduct'] . '</a></h6>';
-        echo '            <div class="product__item__price">R$ ' . $row['price'] . '</div>';
-        echo '            <div class="cart_add">';
-        echo '<a href="api/addProduct.php?id=' . $row['idProduct'] . '">Adicionar ao carrinho</a>';
-        echo '            </div>';
-        echo '        </div>';
-        echo '    </div>';
-        echo '</div>';
+        // ... (código para exibir os produtos)
     }
     echo '</div>';
 
-    // Calcula o número total de produtos após a consulta
-    $totalProdutos = $db->query("SELECT COUNT(*) as total FROM Products WHERE active=1")->fetch_assoc()['total'];
-    $totalPaginas = ceil($totalProdutos / $itensPorPagina);
-
     // Adiciona links de navegação
     echo '<div class="shop__pagination">';
-    for ($i = 1; $i <= $totalPaginas; $i++) {
+    for ($i = 1; $i <= ceil($totalProdutos / $itensPorPagina); $i++) {
         // Adiciona os parâmetros existentes na URL
         $parametrosURL = http_build_query(array_merge($_GET, ['pagina' => $i]));
         echo '<a href="?' . $parametrosURL . '">' . $i . '</a>';
@@ -65,5 +51,6 @@ if ($result) {
 }
 
 // Fecha a conexão com o banco de dados
+$stmt->close();
 $db->close();
 ?>
--- js/providers/providerShop.js
+++ js/providers/providerShop.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
     // Defina a função para a solicitação AJAX
-    function fetchProducts(page) {
+    function fetchProducts() {
         $.ajax({
             url: "api/Products.php",
             method: "GET",
-            data: { pagina: page }, // Adicione o parâmetro de página aqui
             success: function(response) {
                 $("#dataShop").html(response);
             },
@@ -14,13 +13,6 @@ $(document).ready(function() {
         });
     }
 
-    // Chame a função para disparar a solicitação AJAX com a página inicial (1)
-    fetchProducts(1);
-
-    // Adicione um listener para os links de paginação
-    $(document).on("click", ".shop__pagination a", function(e) {
-        e.preventDefault();
-        var page = $(this).text(); // Obtenha o número da página do link clicado
-        fetchProducts(page);
-    });
+    // Chame a função para disparar a solicitação AJAX
+    fetchProducts();
 });
