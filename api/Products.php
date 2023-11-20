6dfb9670e25c1f9119cf83b87efb444e20cc6023
marcotulionasc <marconascpriv@gmail.com>
2023-11-19 21:45:16 -0300

    testando

--- api/Products.php
+++ api/Products.php
@@ -9,7 +9,7 @@ require_once 'connection.php';
 $produtosPorPagina = 8;
 
 // Obtém o número da página atual a partir do parâmetro da URL, se não estiver definido, assume a página 1
-$paginaAtual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+$paginaAtual = isset($_GET['?pagina']) ? $_GET['?pagina'] : 1;
 
 // Calcula o offset para a consulta SQL
 $offset = ($paginaAtual - 1) * $produtosPorPagina;
@@ -49,7 +49,7 @@ if ($result) {
 
     echo '<div class="shop__pagination">';
     for ($i = 1; $i <= $totalPaginas; $i++) {
-        echo '<a href="?pagina=' . $i . '"></a>';
+        echo '<a href="?pagina=' . $i . '">' . $i . '</a>';
     }
     echo '</div>';
 } else {
